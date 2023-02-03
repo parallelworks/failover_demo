8024b61a1797e34eab103726c41a4829764cf558
avidalto <avidalto@hawk.iit.edu>
2023-02-03 11:22:40 -0600

    Add retries special parameter for parsl_utils

--- main.py
+++ main.py
@@ -9,56 +9,7 @@ import parsl_utils
 from parsl_utils.config import config, exec_conf, pwargs, job_number
 from parsl_utils.data_provider import PWFile
 
-from workflow_apps import resilient_app, RetryHandlerConf
-
-# FIXME: This function may need to move to parsl_utils
-def retry_handler(exception, task_record):
-    func_name =  task_record['func_name']
-
-    # FIXME: These lines are needed if the walltime parameter is set in the parsl app
-    #        because of this issue in Parsl https://github.com/Parsl/parsl/issues/2449
-    if func_name == 'wrapper':
-        if 'func_name' in task_record['kwargs']:
-            func_name = task_record['kwargs']['func_name']
-    ############################ end of issue 2449 fix #############################
-    print('\nRetrying function {}'.format(func_name), flush = True)
-    print('Fail history:', flush = True)
-    print(task_record['fail_history'], flush = True)
-    print('Resource session messages:', flush = True)
-    rmsgs = parsl_utils.resource_info.get_resource_messages(exec_conf[task_record['executor']]['POOL'])
-    [ print(msg, flush = True) for msg in rmsgs ]
-
-    if func_name in RetryHandlerConf:
-        print('Retry configuration found for function {func_name}'.format(func_name = func_name), flush = True)
-        fRetryHandlerConf = RetryHandlerConf[func_name]
-    else:
-        print('WARNING: No retry configuration found for function {func_name}!'.format(func_name = func_name), flush = True)
-        return 1
-
-    if type(fRetryHandlerConf) == list:
-        fail_count = task_record['fail_count']
-        print('Fail count: ', fail_count, flush = True)
-        # The first item in fRetryHandlerConf is not considered a retry! (-1)
-        if  fail_count <= len(fRetryHandlerConf) - 1:
-            nfRetryHandlerConf=fRetryHandlerConf[fail_count]
-        else:
-            print('WARNING: No more retries are defined for this function. Using latest retry!', flush = True)
-            nfRetryHandlerConf=fRetryHandlerConf[-1]
-
-        print('Resubmitting with parameters:', flush = True)
-        print(json.dumps(nfRetryHandlerConf, indent = 4), flush = True)
-        task_record['executor'] = nfRetryHandlerConf['executor']
-        task_record['args'] = nfRetryHandlerConf['args']
-        task_record['kwargs'] = nfRetryHandlerConf['kwargs']
-        return 1
-    else:
-        print('ERROR: fRetryHandlerConf was expected to be list and is type={rhctype}'.format(rhctype = str(type(fRetryHandlerConf))), flush = True)
-        return 99999
-
-    return 1
-
-
-
+from workflow_apps import resilient_app
 
 if __name__ == '__main__':
     # Add sandbox directory
@@ -68,7 +19,26 @@ if __name__ == '__main__':
 
     print('\n\nRUNNING PYTHON APP', flush = True)
     retry_app_fut = resilient_app(
-        *RetryHandlerConf['resilient_app'][0]['args'],
-        **RetryHandlerConf['resilient_app'][0]['kwargs']
+        name = 'DivisionByZero', 
+        fail = True
+        retry_params = [
+        {
+            'executor': 'myexecutor_1',
+            'args': ['Timeout'],
+            'kwargs': {
+                'sleep_time': 70,
+                'fail': False,
+                'func_name': 'retry_app_fut'
+            }
+        },
+        {
+            'executor': 'myexecutor_1',
+            'args': ['Success'],
+            'kwargs': {
+                'sleep_time': 1,
+                'fail': False,
+                'func_name': 'retry_app_fut'
+            }
+        }]
     )
     print(retry_app_fut.result())
--- workflow_apps.py
+++ workflow_apps.py
@@ -1,49 +1,12 @@
 from parsl.app.app import python_app, bash_app
 import parsl_utils
 
-
-
-# First item in the lists is used in the first attempt (before any retries)
-# and the rest of the items are used in the retries (second item is retry 1 and so on)
-# - FIXME: If walltime parameter is passed we also need to pass func_name due to this issue in Parsl: https://github.com/Parsl/parsl/issues/2449
-RetryHandlerConf = {
-    "resilient_app": [
-        {
-            'executor': 'myexecutor_1',
-            'args': ['DivisionByZero'],
-            'kwargs': {
-                'sleep_time': 70,
-                'fail': True,
-                'func_name': 'hello_python_app_1'
-            }
-        },
-        {
-            'executor': 'myexecutor_1',
-            'args': ['Timeout'],
-            'kwargs': {
-                'sleep_time': 70,
-                'fail': False,
-                'func_name': 'hello_python_app_1'
-            }
-        },
-        {
-            'executor': 'myexecutor_1',
-            'args': ['Success'],
-            'kwargs': {
-                'sleep_time': 1,
-                'fail': False,
-                'func_name': 'hello_python_app_1'
-            }
-        }
-    ]
-}
-
 # PARSL APPS:
 @parsl_utils.parsl_wrappers.log_app
-@python_app(executors=[RetryHandlerConf['resilient_app'][0]['executor']])
-def resilient_app(name, sleep_time = 10, fail = False, stdout='std.out', stderr = 'std.err', walltime=10, func_name = 'resilient_app'):
+@python_app(executors=['myexecutor_1'])
+def resilient_app(name, sleep_time = 10, fail = False, stdout = 'std.out', stderr = 'std.err', walltime = 10, func_name = 'resilient_app'):
     """
-    App to model the following scenairos:
+    App to model the following scenarios:
     1. Failure: if Fail=True), 
     2. Timeout: if sleep_time > walltime
     3. Successul completion
