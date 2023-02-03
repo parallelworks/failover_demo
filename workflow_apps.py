5c08c4c95d10149f10cc30da9aeca4c8340fbf5d
avidalto <avidalto@hawk.iit.edu>
2023-02-03 12:44:57 -0600

    Fix bugs

--- workflow_apps.py
+++ workflow_apps.py
@@ -1,29 +1,44 @@
-from parsl.app.app import python_app, bash_app
+import sys, os, json, time
+from random import randint
+import argparse
+
+import parsl
+print(parsl.__version__, flush = True)
+
 import parsl_utils
+from parsl_utils.config import config, exec_conf, pwargs, job_number
+from parsl_utils.data_provider import PWFile
 
-# PARSL APPS:
-@parsl_utils.parsl_wrappers.log_app
-@python_app(executors=['myexecutor_1'])
-def resilient_app(name, sleep_time = 10, fail = False, stdout = 'std.out', stderr = 'std.err', walltime = 10, func_name = 'resilient_app'):
-    """
-    App to model the following scenarios:
-    1. Failure: if Fail=True), 
-    2. Timeout: if sleep_time > walltime
-    3. Successul completion
-    """
-    import socket
-    from time import sleep
-    from datetime import datetime
+from workflow_apps import resilient_app
 
-    if fail:
-        _ = 1/0
+if __name__ == '__main__':
+    # Add sandbox directory
 
-    for i in range(sleep_time):
-        f = open('/tmp/time.out', 'a')
-        f.write(str(i) + '\n')
-        f.close()
-        time.sleep(1)
+    print('\n\nLOADING PARSL CONFIG', flush = True)
+    parsl.load(config)
 
-    if not name:
-        name = 'python_app_1'
-    return 'Hello ' + name + ' from ' + socket.gethostname()
+    print('\n\nRUNNING PYTHON APP', flush = True)
+    retry_app_fut = resilient_app(
+        'DivisionByZero', 
+        fail = True,
+        retry_parameters = [
+        {
+            'executor': 'myexecutor_1',
+            'args': ['Timeout'],
+            'kwargs': {
+                'sleep_time': 70,
+                'fail': False,
+                'func_name': 'retry_app_fut'
+            }
+        },
+        {
+            'executor': 'myexecutor_1',
+            'args': ['Success'],
+            'kwargs': {
+                'sleep_time': 1,
+                'fail': False,
+                'func_name': 'retry_app_fut'
+            }
+        }]
+    )
+    print(retry_app_fut.result())
