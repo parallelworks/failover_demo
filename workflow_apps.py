47a354007bdf3fda6f74dbffb053e1888b4945f7
avidalto <avidalto@hawk.iit.edu>
2023-02-08 10:46:48 -0600

    Test killing the app

--- workflow_apps.py
+++ workflow_apps.py
@@ -1,20 +1,35 @@
 from parsl.app.app import python_app, bash_app
 import parsl_utils
+import socket
+import os
+
 
 # PARSL APPS:
 @parsl_utils.parsl_wrappers.log_app
 @python_app(executors=['myexecutor_1'])
-def resilient_app(name, sleep_time = 10, fail = False, walltime = 20, retry_parameters = None, func_name = 'resilient_app'):
+def resilient_app(name, sleep_time = 10, fail = False, walltime = 300, retry_parameters = None, func_name = 'resilient_app'):
     """
     App to model the following scenarios:
     1. Failure: if Fail=True), 
     2. Timeout: if sleep_time > walltime
     3. Successul completion
     """
-    import socket
+
+    import socket, os
     from time import sleep
 
     import logging
+
+    def register_app(app_id):
+        with open(app_id + '.pid', 'w') as f:
+            f.write(str(os.getpid()))
+            
+        with open(app_id + '.host', 'w') as f:
+            f.write(socket.gethostname())
+
+    
+    register_app('my-app')
+
     formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
     
     def get_logger(log_file, name, level = logging.INFO):
